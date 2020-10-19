68313da2472574ac15c640bb37ece7548319fa2f
monstaro <codysmith2814@gmail.com>
2020-10-19 16:33:30 -0600

    Consrtucts basic css

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
+var metronome = new Metronome();
+var tempo = document.getElementById('tempo');
+tempo.textContent = metronome.tempo;
 
-const audioElement = document.querySelector('audio');
+var playPauseIcon = document.getElementById('play-pause-icon');
 
-const track = audioContext.createMediaElementSource(audioElement);
-
-track.connect(audioContext.destination);
-
-const playButton = document.querySelector('button');
-
+var playButton = document.getElementById('play-button');
 playButton.addEventListener('click', function() {
+    metronome.startStop();
 
-    // check if context is in suspended state (autoplay policy)
-    if (audioContext.state === 'suspended') {
-        audioContext.resume();
+    if (metronome.isRunning) {
+        playPauseIcon.className = 'pause';
+    }
+    else {
+        playPauseIcon.className = 'play';
     }
 
-    // play or pause track depending on state
-    if (this.dataset.playing === 'false') {
-        audioElement.play();
-        this.dataset.playing = 'true';
-    } else if (this.dataset.playing === 'true') {
-        audioElement.pause();
-        this.dataset.playing = 'false';
-    }
+});
 
-}, false);
-
-audioElement.addEventListener('ended', () => {
-    playButton.dataset.playing = 'false';
-}, false);
+var tempoChangeButtons = document.getElementsByClassName('tempo-change');
+for (var i = 0; i < tempoChangeButtons.length; i++) {
+    tempoChangeButtons[i].addEventListener('click', function() {
+        metronome.tempo += parseInt(this.dataset.change);
+        tempo.textContent = metronome.tempo;
+    });
+}
